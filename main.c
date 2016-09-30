8ea5ef14ca3e8bb80e5cb1725b6fbbf03d596ed8
Abderrahmane-H <abderrahmane-hilali@outlook.com>
2016-09-30 21:51:26 +0100

    verification added

--- main.c
+++ main.c
@@ -30,11 +30,54 @@ int isSign(char c)
     return(0);
 }
 
+/**
+ * @brief verifier si un caratere est un point ou non 
+ * @param c : le caractere a verifier
+ * @return 0 si le caractere n'est pas un point
+ *         1 si le caractere est un point
+ */
+int isPoint(char c)
+{
+     //le caractere c est un point
+    if( ( c == '+' ) || ( c == '-' ) ) 
+        return(1);
+    //le caractere c n'est pas une signe
+    return(0);       
+}
+
+
 int main(int argc, char **argv)
 {
 	
+    //declaration des variables
+    int partieE = 0; //la partie entiere du nombre
+    int partieF = 0; // la partie flottante
+    char courant;    // le caractere courant dans le buffer
+    char signe = ' '; //le signe du nombre
     
+    //premierment en lire le nombre 
+    printf("Entrez un nombre");
+    courant = getchar();
     
+    //verifier la validite du caractere courant
+    if( !isNumber(courant) || !isSign(courant) || !isPoint(courant) )
+    {
+        //on affiche un message d'erreur
+        printf("erreur entrer un nombre valide");
+        return(0);
+    }
+    
+    //si le caractere courant est un signe
+    if( isSign(courant) )
+        //on sauvgarde le signe dans une variable
+        signe = courant;
+        
+    //si le caractere courant est un nombre
+    if( isNumber(courant) )
+        //on calcule sa valuer puit on lajout a partieE
+        partieE = courant - '0';
+    
+    if( isPoint(courant) )
     
 	return 0;
 }
