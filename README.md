907e448fca2e61dce1d988c26c127e49a70b36d0
Abderrahmane-H <abderrahmane-hilali@outlook.com>
2016-09-30 21:27:24 +0100

    functions added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # le-compte-est-bon
+
+
+# steps to follow to create this program :
+  1- read a charactere
+	-verify the validity of that caractere,
+ 	-if it is valid then :
+		- check if it is a '+', '-' or '.' or a number,
+		- if it is a sign : we store that signe in a variable
+		- if it is a point we start calculating the 
--- main.c
+++ main.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 
+/**
+ * @brief verifier si un caratere est nombre ou non 
+ * @param c : le caractere a verifier
+ * @return  0 si le caractere n'est pas un nomre
+ *          1 si le caractere est un nombre
+ **/
+int isNumber(char c)
+{
+    //le caractere c n'est un nombre
+    if( ( c > '9' ) || ( c < '0' ) ) 
+        return(0);
+    //le caractere c est pas un nombre
+    return(1);
+}
+
+/**
+ * @brief verifier si un caratere est une signe ou non 
+ * @param c : le caractere a verifier
+ * @return 0 si le caractere n'est pas une signe
+ *         1 si le caractere est une signe
+ */
+int isSign(char c)
+{
+    //le caractere c est une signe
+    if( ( c == '+' ) || ( c == '-' ) ) 
+        return(1);
+    //le caractere c n'est pas une signe
+    return(0);
+}
+
 int main(int argc, char **argv)
 {
-	printf('');
+	
+    
+    
+    
 	return 0;
 }
